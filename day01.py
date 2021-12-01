5cfa834b1d6c24b76072f617647e37bfd2b648df
Daniel Siemssen <daniel@dr-siemssen.de>
2021-12-01 21:26:48 +0100

    Small changes to make the linter happy.

--- day01.py
+++ day01.py
@@ -1,4 +1,4 @@
-with open('day01.txt') as file:
-    input = list(map(int, file))
-    for d in 1,3:
-        print(sum(x < y for x, y in zip(input, input[d:])))
+with open('day01.txt', encoding='utf8') as file:
+    data = list(map(int, file))
+    for d in 1, 3:
+        print(sum(x < y for x, y in zip(data, data[d:])))
