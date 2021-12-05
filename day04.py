681e06c70ad01a4fa1d8ec966a284f144fae1914
Daniel Siemssen <daniel@dr-siemssen.de>
2021-12-05 13:53:17 +0100

    Small improvements of days 4 and 5

--- day04.py
+++ day04.py
@@ -4,16 +4,11 @@ with open('day04.txt', encoding='utf8') as file:
     nums, *boards = file
 boards = np.loadtxt(boards, dtype=np.uint8).reshape(-1, 5, 5)
 
-part = 0
-scores = [0, 0]
-for n in np.uint8(nums.split(',')):
+for n in map(int, nums.split(',')):
     boards[boards == n] = 255
     cond = boards == 255
-
     won = (cond.all(1) | cond.all(2)).any(1)
     if won.any():
-        scores[part] = n * np.sum(~cond[won] * boards[won], dtype=int)
+        if boards.shape[0] in (1, 100):
+            print(np.sum(n * ~cond[won] * boards[won]))
         boards = boards[~won]
-        part = 1
-
-print(*scores)
